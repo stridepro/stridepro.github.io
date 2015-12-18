fc55d93dde4a620ae74dfe1e7bc64d9bf1b3f83e
Alandra Greenlee <alandra.green@gmail.com>
2015-12-18 14:43:44 -0500

    Update main1.js

--- assets/js/main1.js
+++ assets/js/main1.js
@@ -39,7 +39,7 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.jpg': 'center',
+							'http://spectorjosh.com/wp-content/uploads/2014/10/bottom-of-pool.jpg': 'center',
 							'images/bg02.jpg': 'center',
 							'images/bg03.jpg': 'center'
 						},
