c8b468be6e6b15cae898a0c27d9b763dc236c0bc
Alandra Greenlee <alandra.green@gmail.com>
2015-12-19 07:54:30 -0500

    Update main1.js

--- assets/js/main1.js
+++ assets/js/main1.js
@@ -40,6 +40,8 @@
 					// Images (in the format of 'url': 'alignment').
 						images: {
 							'images/bg01.jpg': 'center',
+							'http://daymanspools.com/wp-content/uploads/2012/06/1448028341.jpg': 'center',
+							'http://www2.pictures.zimbio.com/gi/Shannon+Vreeland+2012+Olympic+Swimming+Team+42jHvNNOax4l.jpg':'center'
 							'images/bg03.jpg': 'center'
 						},
 
